52b7dab5ea8d0168665422bb28759a8ea7de543f
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-07-15 01:21:42 -0700

    Make the refreshUI function within main function to make the projects and tasks on the webpage

--- src/index.js
+++ src/index.js
@@ -12,7 +12,17 @@ function main(){
     const tasksListContainer = document.querySelector("ul#task-list");
     const taskHeading = document.querySelector(".tasks-heading");
 
-    
+    function refreshUI(){
+        const allProjects = projectsModule.projects();
+        const activeProject = projectsModule.getActiveProject();
+
+        if(projectsListContainer){
+            renderProjects(allProjects);
+        }
+        if(tasksListContainer && activeProject){
+            displayTasks(activeProject["todos"]);
+        }
+    }
 
 
 
